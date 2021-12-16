5e105dd0fae982be1d516a0609e70dece280ef77
Zack Sorrell <zackssorrell24@gmail.com>
2021-12-16 11:00:17 -0700

    updated

---
+++ Algos_Week1/day3.js
@@ -0,0 +1,26 @@
+//Functions
+//1. Defining the function is making the box and writing the fuuction
+//2. Calling the function (push button, ect.)
+
+//When using parameters you need the eqaul amount of arguments
+//Paarameters = variables
+//Arguments = data or information being passed into the parameters
+
+function print1to50() {
+    for(var i = 0; i <51; i++) //code body
+        console.log(i)
+}
+
+//Define the function
+function add(num1,num2) {
+    return num1 + num2
+}
+
+//Call the function
+add()
+
+//Rules of Return statements
+//1. You can only return one thing when using return
+//2. A return statement immediately stops the function
+
+
