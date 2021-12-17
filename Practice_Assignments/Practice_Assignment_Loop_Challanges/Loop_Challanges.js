63d62865bf345361f83c538b3f23d0d14fdddeb2
Zack Sorrell <zackssorrell24@gmail.com>
2021-12-16 19:52:56 -0700

    updated

--- Practice_Assignments/Practice_Assignment_Loop_Challanges/Loop_Challanges.js
+++ Practice_Assignments/Practice_Assignment_Loop_Challanges/Loop_Challanges.js
@@ -16,5 +16,29 @@ for(var i = 100; i >= 0; i--){
 
 //Print the sequence
 
+for(var i=4; i>-4; i-=1.5) {
+    console.log(i);
+}
 
+//Sigma
+
+var sum = 0
+
+for(var i = 1; i < 101; i++){
+    if(i < 101){
+        var sum = sum + i
+    }    
+}
+    console.log(sum)
+
+//Factorial
+
+var product = 1
+
+for(var i = 1; i < 13; i++){
+    if(i < 13){
+        product = product * i
+    }
+}
+    console.log(product)
 
