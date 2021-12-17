d7c2ea075c6089ccfe8ed0976b12ed8d49d00fde
Zack Sorrell <zackssorrell24@gmail.com>
2021-12-16 18:47:34 -0700

    added

---
+++ Practice_Assignments/Practice_Assignment_Loop_Challanges/Loop_Challanges.js
@@ -0,0 +1,20 @@
+//Print Odds 1-20
+
+for(i = 1; 1 < 21; i++){
+    if(i % 2 !== 0){
+        console.log(i)
+    }
+}
+
+//Decreasing Multiples of 3
+
+for(i = 100; i >= 0; i--){
+    if(i % 3 == 0){
+        console.log(i)
+    }
+}
+
+//Print the sequence
+
+
+
