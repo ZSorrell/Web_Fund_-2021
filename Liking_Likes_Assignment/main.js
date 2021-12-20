2e246dadedbf64e398b767d3f802b5b8972b4a9c
Zack Sorrell <zackssorrell24@gmail.com>
2021-12-20 15:54:58 -0700

    updated

--- Liking_Likes_Assignment/main.js
+++ Liking_Likes_Assignment/main.js
@@ -3,7 +3,7 @@ var numberLikes = 9
 function add(){
     var likes = document.querySelector(".number-of-likes-one").innerText
     numberLikes++
-    likes = numberLikes + " likes(s)"
+    likes = numberLikes + " like(s)"
     console.log(likes)
     document.querySelector(".number-of-likes-one").innerText = likes
 }
@@ -13,7 +13,7 @@ var numberLikesTwo = 12
 function addtwo(){
     var likestwo = document.querySelector(".number-of-likes-two").innerText
     numberLikesTwo++
-    likestwo = numberLikesTwo + " likes(s)"
+    likestwo = numberLikesTwo + " like(s)"
     console.log(likestwo)
     document.querySelector(".number-of-likes-two").innerText = likestwo
 }
@@ -23,7 +23,7 @@ var numberLikesThree = 9
 function addthree(){
     var likesthree = document.querySelector(".number-of-likes-three").innerText
     numberLikesThree++
-    likesthree = numberLikesThree + " likes(s)"
+    likesthree = numberLikesThree + " like(s)"
     console.log(likesthree)
     document.querySelector(".number-of-likes-three").innerText = likesthree
 }
