528e773b0d9bc9d579a2a9fb5da882eefed88885
Zack Sorrell <zackssorrell24@gmail.com>
2021-12-20 16:02:48 -0700

    updated

--- Button_Clicker_Assignment/main.js
+++ Button_Clicker_Assignment/main.js
@@ -1,4 +1,11 @@
-function onclick(add){
-    document.querySelector(number-of-likes-one)
-    add++
+function Logout(element){
+    element.innerText = "Logout"
+}
+
+function onclick(alert){
+    alert("Ninja was liked!")
+}
+
+function hide(element){
+    element.remove()
 }
