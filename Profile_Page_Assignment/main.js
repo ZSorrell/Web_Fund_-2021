02c2eda648a5b735af4cc40409a3acc81022789e
Zack Sorrell <zackssorrell24@gmail.com>
2021-12-21 16:34:09 -0700

    updated

--- Profile_Page_Assignment/main.js
+++ Profile_Page_Assignment/main.js
@@ -1,6 +1,18 @@
-var profileLink = document.querySelector(".bottom-info-box-profile-full-name")
+function hideOne(){
+var declineButtonOne = document.querySelector(".top-connection-person-picture-name");
+var acceptButtonOne = document.querySelector(".top-connection-person-picture-name");
+declineButtonOne.remove();
+acceptButtonOne.remove();
+}
+
+function hideTwo(){
+    var declineButtonTwo = document.querySelector(".bottom-connection-person-picture-name");
+    var acceptButtonTwo = document.querySelector(".bottom-connection-person-picture-name");
+    declineButtonTwo.remove();
+    acceptButtonTwo.remove();
+}
 
 function changeName(){
-    var profileLink = document.querySelector(".bottom-info-box-profile-full-name")
-    profileLink.innerText
+    var profileName = document.querySelector(".bottom-info-box-profile-full-name");
+    profileName.innerText = "Zack Sorrell"
 }
