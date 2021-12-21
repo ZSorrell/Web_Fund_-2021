30762e750cfc644d5b4793031613bbf944cf5b71
Zack Sorrell <zackssorrell24@gmail.com>
2021-12-21 15:59:06 -0700

    updated

--- Practice_Assignments/Practice_Assignment_Dojo_Weather/main.js
+++ Practice_Assignments/Practice_Assignment_Dojo_Weather/main.js
@@ -1,8 +1,16 @@
 function message(){
-    
+    alert("Loading weather report...")
 }
 
 function hide(){
+    var cookiealert = document.querySelector("#cookie-alert")
+    cookiealert.remove()
+}
+
+function swapToFar(){
+    var degreeOne = document.querySelectorAll("degree-one")
+    var degreeTwo = document.querySelectorAll("degree-two")
+    var selector = document.querySelectorAll("temperature")
 
 }
 
