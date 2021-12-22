143922b07848e112689ccc72f8b1b75299c09a25
Zack Sorrell <zackssorrell24@gmail.com>
2021-12-22 15:07:49 -0700

    updated

--- Belt_Exam_First_Zack_Sorrell/main.js
+++ Belt_Exam_First_Zack_Sorrell/main.js
@@ -1,1 +1,16 @@
-console.log("Hello World")
+function hide(){
+    var acceptButton = document.querySelector("#plant-alert");
+    acceptButton.remove();
+}
+
+function shoppingCart(){
+    alert("Your cart is empty")
+}
+
+function newPic(){
+    document.getElementById("event").src = "images/succulents-2.jpg"
+}
+
+function oldPic(){
+    document.getElementById("event").src = "images/succulents-1.jpg"
+}
